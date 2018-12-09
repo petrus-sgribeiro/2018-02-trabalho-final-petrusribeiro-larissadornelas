c7d986cc26e4d559685fd5304737992779022996
Petrus Ribeiro <petrus.sgribeiro@gmail.com>
2018-12-09 17:03:57 -0200

    Admin - Produtos

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Latrus\Page;
 use \Latrus\PageAdmin;
 use \Latrus\Model\User;
 use \Latrus\Model\Category;
+use \Latrus\Model\Product;
 
 $app = new \Slim\Slim();
 
@@ -330,6 +331,96 @@ $app->get("/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/admin/products", function(){
+
+	User::verifyLogin();
+
+	$products = Product::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", [
+		"products"=>$products
+
+	]);
+
+});
+
+$app->get("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-create");
+
+});
+
+$app->post("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->setData($_POST);
+
+	$product->save();
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-update", [
+		'product'=>$product->getValues()
+
+	]);
+
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$product->setData($_POST);
+
+	$product->save();
+
+	$product->setPhoto($_FILES["file"]);
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$product->delete();
+
+	header("Location: /admin/products");
+	exit;
+
+});
 
 $app->run();
 
